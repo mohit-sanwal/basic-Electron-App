3321e258f5cab862e8ea5d0e4b7fb5d4f81493f7
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-15 18:23:54 +0530

    drag and drop image functionality implimentation

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow, ipcMain  } = require('electron');
+const { app, BrowserWindow, ipcMain,  } = require('electron');
 const path = require('path')
+const fs = require('fs')
+const https = require('https')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -12,16 +14,48 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
-  createWindow()
 
-  app.on('activate', () => {
-    if(BrowserWindow.getAllWindows().length === 0) {createWindow()}
+const iconName = path.join(__dirname, 'iconForDragAndDrop.png')
+const icon = fs.createWriteStream(iconName)
+
+// Create a new file to copy - you can also copy existing files.
+fs.writeFileSync(path.join(__dirname, 'drag-and-drop-1.md'), '# First file to test drag and drop')
+
+
+ipcMain.on('ondragstart', (event, filePath) => {
+  event.sender.startDrag({
+    file: path.join(__dirname, filePath),
+    icon: iconName
   })
 })
 
+https.get('https://img.icons8.com/ios/452/drag-and-drop.png', (response) => {
+  response.pipe(icon)
+})
+
+// const NOTIFICATION_TITLE = 'Basic Notification'
+// const NOTIFICATION_BODY = 'Notification from the Main process'
+
+// function showNotification () {
+//   new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+// }
+// app.whenReady().then(createWindow).then(showNotification);
+
+app.whenReady().then(createWindow);
+
+// app.whenReady().then(() => {
+//   ipcMain.handle('ping', () => 'pong')
+//   createWindow()
+
+//   app.on('activate', () => {
+//     if(BrowserWindow.getAllWindows().length === 0) {createWindow()}
+//   })
+// })
+
 app.on('window-all-closed', () => {
-    
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
+})
+
+app.on('activate', () => {
+  if(BrowserWindow.getAllWindows().length === 0) {createWindow()}
 })
--- preload.js
+++ preload.js
@@ -1,9 +1,21 @@
 const { contextBridge, ipcRenderer } = require('electron')
+// const path = require('path')
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping')
-  // we can also expose variables, not just functions
+// contextBridge.exposeInMainWorld('versions', {
+//   node: () => process.versions.node,
+//   chrome: () => process.versions.chrome,
+//   electron: () => process.versions.electron,
+//   ping: () => ipcRenderer.invoke('ping'),
+//   startDrag: (fileName) => {
+//     ipcRenderer.send('ondragstart', fileName)
+//   }
+  
+//   // we can also expose variables, not just functions
+// })
+
+
+contextBridge.exposeInMainWorld('electron', {
+ startDrag: (fileName) => {
+    ipcRenderer.send('ondragstart', fileName)
+  }
 })
