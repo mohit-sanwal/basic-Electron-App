4e83698efd7d9b73822a975649f63503f82cd94b
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-19 23:45:53 +0530

    main and renderer and preload file added for image resizer

--- main.js
+++ main.js
@@ -1,90 +1,39 @@
-const { app, BrowserWindow, ipcMain, dialog, globalShortcut, Tray, Menu} = require('electron');
+// resize image
+
+const { app, BrowserWindow} = require('electron');
 const path = require('path')
 const fs = require('fs')
 const https = require('https')
 
-  // ADDING MENU
-  let isMac = process.platform === 'darwin'
-  let templateMwnu = 
-  [ 
-    ...isMac ? {label: 'Blog', submenu: [{label: 'About'},{label: 'Version'}]} : [],
-    {label: 'File'},
-    {label: 'operation', 
-    submenu: [isMac ? {role: 'close', label: 'Close'} : {role: 'quit', label: 'Quit'},{label: 'Zoom'}]}
-  ]
-  const menu = Menu.buildFromTemplate(templateMwnu);
-  Menu.setApplicationMenu(menu);
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
+const isDev = process.env.NODE_ENV !== 'production'
+const isMac = process.platform === 'darwin'
+let mainWindow;
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: isDev ? 1200: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
         preload: path.join(__dirname, 'preload.js')
     }
   })
-   console.log('hello', path.join(__dirname, 'preload.js'))
-  win.loadFile('index.html')
-  // add tray
-  tray = new Tray('ex.png');
-  tray.setToolTip("try electron app..")
-  // tray.on('click', ()=> {
-  //   win.isVisible() ? win.hide() : win.show()
-  // })
-  let template = [{label: 'item1', type: 'radio'},{label: 'item2', type: 'radio'}]
-  const contextMenu = Menu.buildFromTemplate(template);
-  tray.setContextMenu(contextMenu);
-  globalShortcut.register('Shift + K', () => {
-    dialog.showOpenDialog({
-      defaultPath: app.getPath('desktop'),
-      buttonLabel: 'select file'
-    });
-  })
 
+if(isDev) {
+  mainWindow.webContents.openDevTools()
+}
+
+mainWindow.loadFile('renderer/index.html')
 
 }
 
-
-const iconName = path.join(__dirname, 'iconForDragAndDrop.png')
-const icon = fs.createWriteStream(iconName)
-
-// Create a new file to copy - you can also copy existing files.
-fs.writeFileSync(path.join(__dirname, 'drag-and-drop-1.md'), '# First file to test drag and drop')
+app.whenReady().then(createMainWindow);
 
 
-ipcMain.on('ondragstart', (event, filePath) => {
-  event.sender.startDrag({
-    file: path.join(__dirname, filePath),
-    icon: iconName
-  })
-})
-
-https.get('https://img.icons8.com/ios/452/drag-and-drop.png', (response) => {
-  response.pipe(icon)
-})
-
-// const NOTIFICATION_TITLE = 'Basic Notification'
-// const NOTIFICATION_BODY = 'Notification from the Main process'
-
-// function showNotification () {
-//   new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
-// }
-// app.whenReady().then(createWindow).then(showNotification);
-
-app.whenReady().then(createWindow);
-
-// app.whenReady().then(() => {
-//   ipcMain.handle('ping', () => 'pong')
-//   createWindow()
-
-//   app.on('activate', () => {
-//     if(BrowserWindow.getAllWindows().length === 0) {createWindow()}
-//   })
-// })
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 })
-
+  
 app.on('activate', () => {
   if(BrowserWindow.getAllWindows().length === 0) {createWindow()}
 })
--- preload.js
+++ preload.js
@@ -1,21 +1,45 @@
-const { contextBridge, ipcRenderer } = require('electron')
-// const path = require('path')
+// const { contextBridge, ipcRenderer } = require('electron')
+// // const path = require('path')
 
-// contextBridge.exposeInMainWorld('versions', {
-//   node: () => process.versions.node,
-//   chrome: () => process.versions.chrome,
-//   electron: () => process.versions.electron,
-//   ping: () => ipcRenderer.invoke('ping'),
-//   startDrag: (fileName) => {
+// // contextBridge.exposeInMainWorld('versions', {
+// //   node: () => process.versions.node,
+// //   chrome: () => process.versions.chrome,
+// //   electron: () => process.versions.electron,
+// //   ping: () => ipcRenderer.invoke('ping'),
+// //   startDrag: (fileName) => {
+// //     ipcRenderer.send('ondragstart', fileName)
+// //   }
+  
+// //   // we can also expose variables, not just functions
+// // })
+
+
+// contextBridge.exposeInMainWorld('electron', {
+//  startDrag: (fileName) => {
 //     ipcRenderer.send('ondragstart', fileName)
 //   }
-  
-//   // we can also expose variables, not just functions
 // })
 
 
-contextBridge.exposeInMainWorld('electron', {
- startDrag: (fileName) => {
-    ipcRenderer.send('ondragstart', fileName)
-  }
-})
+const os = require('os');
+const path = require('path');
+const { contextBridge, ipcRenderer } = require('electron');
+const Toastify = require('toastify-js');
+
+contextBridge.exposeInMainWorld('os', {
+  homedir: () => os.homedir(),
+});
+
+contextBridge.exposeInMainWorld('path', {
+  join: (...args) => path.join(...args),
+});
+
+contextBridge.exposeInMainWorld('ipcRenderer', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, func) =>
+    ipcRenderer.on(channel, (event, ...args) => func(...args)),
+});
+
+contextBridge.exposeInMainWorld('Toastify', {
+  toast: (options) => Toastify(options).showToast(),
+});
